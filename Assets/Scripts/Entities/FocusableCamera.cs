7ee7803b7dec9da890de4dd188ff7407a4ad78a1
deep-154 <deepankar11093@gmail.com>
2016-03-01 23:43:31 +0530

    camera script attached for proper fight view

---
+++ Assets/Scripts/Entities/FocusableCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FocusableCamera : MonoBehaviour {
+	public Transform leftTarget;
+	public Transform rightTarget;
+
+	public float minDistance;
+
+	private Vector3 newPosition;
+
+	void Start(){
+		newPosition = new Vector3 ();
+	}
+	
+	void Update () {
+		float distanceBetweenTargets = Mathf.Abs (leftTarget.position.x - rightTarget.position.x);
+		float centerPosition = (leftTarget.position.x + rightTarget.position.x)/ 2;
+
+		newPosition.Set (
+			centerPosition, transform.position.y,
+			distanceBetweenTargets > minDistance? -distanceBetweenTargets: -minDistance);
+
+		transform.position = newPosition;
+	}
+}
