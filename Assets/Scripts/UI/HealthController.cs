07728f40623b999bfe72af2b20b24c3774b2a69c
deep-154 <deepankar11093@gmail.com>
2016-03-04 19:33:28 +0530

    health bars added

---
+++ Assets/Scripts/UI/HealthController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour {
+	public Fighter player1;
+	public Fighter player2;
+
+	public Text player1Tag;
+	public Text player2Tag;
+
+	public Scrollbar leftBar;
+	public Scrollbar rightBar;
+
+	public Text timerText;
+
+	//public BattleController battle;
+
+
+	// Use this for initialization
+	void Start () {
+		player1Tag.text = player1.fighterName;
+		player2Tag.text = player2.fighterName;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*	timerText.text = battle.roundTime.ToString();*/
+
+		if (leftBar.size > player1.healthPercent) {
+			leftBar.size-= 0.01f;
+		}
+		if (rightBar.size > player2.healthPercent) {
+			rightBar.size-= 0.01f;
+		}
+	}
+}
